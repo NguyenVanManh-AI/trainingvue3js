5ea16a792928488f4825476374c710fc936a80b7
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-07-14 20:00:59 +0700

    Prop in Vue

---
+++ src/components/ComponentA.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="comp-a">
+      Component A 
+      <strong>{{ numberChild }}</strong>
+      
+      <br>
+      <strong>{{ arrChild[4].age }}</strong>
+      <br>
+      <strong>{{ arrChild[4].name }}</strong>
+      <br>
+
+
+      <strong>{{ personChild.weight }}</strong>
+      <strong>{{ personChild.age }}</strong>
+      <strong>{{ personChild.name }}</strong>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ComponentA',
+    data() {
+      return {
+
+      }
+    },
+    props : {
+      numberChild: Number,
+      arrChild: Array,
+      personChild: Object,
+      printNameChild: Function,
+    },
+
+    mounted() {
+      this.printNameChild();
+    },
+
+    methods: {
+      // printNameChild: function() {
+
+      // }
+    },
+
+    setup() {
+  
+    },
+    beforeCreate() {
+  
+    },
+    created() {
+  
+    },
+    beforeMount() {
+  
+    },
+
+    beforeUpdate() {
+  
+    },
+    updated() {
+  
+    },
+    beforeUnmount() {
+  
+    },
+    unmounted() {
+      
+    }
+  }
+  </script>
+  
+  <style >
+
+  </style>
+  
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,70 +1,71 @@
 <template>
   <div class="hello">
-    <!-- <img :src="imageSrc" :alt="img_alt" :placeholder="img_alt"> -->
-
-    <p :class="{ 'color-red': (isRed == true), underline: isUnderline == true }">STRING</p>
-
-    <button @click="isRed = !isRed">Change Color</button>
-    <button @click="isUnderline = !isUnderline">Change Style</button>
-
-    <h1>Vue.js Lifecycle Hooks Demo</h1>
-    <button @click="toggleComponent">Toggle Component</button>
-    <div v-if="showChild">Child Component</div>
-  </div>
+    <CompA99999 :numberChild="number" :arrChild="arr" :personChild="person" :printNameChild="printName"></CompA99999>
+</div>
 </template>
 
 <script>
+import CompA from '@/components/ComponentA' 
+// import CompA from './ComponentA'
+
 export default {
   name: 'HelloWorld',
+  components : {
+    CompA99999:CompA,
+  },
   data() {
     return {
-      imageSrc: 'https://media.geeksforgeeks.org/wp-content/uploads/20230111192709/diff.PNG',
-      img_alt: 'Ảnh',
-      showChild: true,
-      isRed: true,
-      isUnderline: true,
+      number : 9,
+      arr: [1,2,3, 'string', {age:19, name:'nvm'}],
+      person : {
+        weight:59,
+        height: 179,
+        age:19, 
+        name:'nvm'
+      }
     }
   },
   methods: {
-    toggleComponent() {
-          this.showChild = !this.showChild;
-        }
+    printName: function() {
+      console.log("Nguyen Van Manh");
+    }
   },
   setup() {
-    console.log('setup');
-    return {};
+
   },
   beforeCreate() {
-    console.log('beforeCreate');
+
   },
   created() {
-    console.log('created');
+
   },
   beforeMount() {
-    console.log('beforeMount');
+
   },
   mounted() {
-    console.log('mounted');
-    var arr = ['tag1', 'tag2', 'tag3', 999]; // ["tag1","tag2","tag3",999]
-    console.log(JSON.stringify(arr));
-    var obj = {
-      age: 29,
-      name: 'Nguyễn Văn Mạnh'
-    };
-    arr = ['tag1', 'tag2', 'tag3', 999, obj]; // ["tag1","tag2","tag3",999]
-    console.log(JSON.stringify(arr)); // ["tag1","tag2","tag3",999,{"age":29,"name":"Nguyễn Văn Mạnh"}]
+
+
+    // console.log('mounted');
+    // var arr = ['tag1', 'tag2', 'tag3', 999]; // ["tag1","tag2","tag3",999]
+    // console.log(JSON.stringify(arr));
+    // var obj = {
+    //   age: 29,
+    //   name: 'Nguyễn Văn Mạnh'
+    // };
+    // arr = ['tag1', 'tag2', 'tag3', 999, obj]; // ["tag1","tag2","tag3",999]
+    // console.log(JSON.stringify(arr)); // ["tag1","tag2","tag3",999,{"age":29,"name":"Nguyễn Văn Mạnh"}]
   },
   beforeUpdate() {
-    console.log('beforeUpdate');
+
   },
   updated() {
-    console.log('updated');
+
   },
   beforeUnmount() {
-    console.log('beforeUnmount');
+
   },
   unmounted() {
-    console.log('unmounted');
+
   }
 }
 </script>
