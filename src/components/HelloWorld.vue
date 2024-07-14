45cc2bf1996f66f69fefeb6540635f80da8adc8c
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-07-14 19:30:05 +0700

    life cycle vuejs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,83 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <!-- <img :src="imageSrc" :alt="img_alt" :placeholder="img_alt"> -->
+
+    <p :class="{ 'color-red': (isRed == true), underline: isUnderline == true }">STRING</p>
+
+    <button @click="isRed = !isRed">Change Color</button>
+    <button @click="isUnderline = !isUnderline">Change Style</button>
+
+    <h1>Vue.js Lifecycle Hooks Demo</h1>
+    <button @click="toggleComponent">Toggle Component</button>
+    <div v-if="showChild">Child Component</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      imageSrc: 'https://media.geeksforgeeks.org/wp-content/uploads/20230111192709/diff.PNG',
+      img_alt: 'Ảnh',
+      showChild: true,
+      isRed: true,
+      isUnderline: true,
+    }
+  },
+  methods: {
+    toggleComponent() {
+          this.showChild = !this.showChild;
+        }
+  },
+  setup() {
+    console.log('setup');
+    return {};
+  },
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+  created() {
+    console.log('created');
+  },
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  mounted() {
+    console.log('mounted');
+    var arr = ['tag1', 'tag2', 'tag3', 999]; // ["tag1","tag2","tag3",999]
+    console.log(JSON.stringify(arr));
+    var obj = {
+      age: 29,
+      name: 'Nguyễn Văn Mạnh'
+    };
+    arr = ['tag1', 'tag2', 'tag3', 999, obj]; // ["tag1","tag2","tag3",999]
+    console.log(JSON.stringify(arr)); // ["tag1","tag2","tag3",999,{"age":29,"name":"Nguyễn Văn Mạnh"}]
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+  updated() {
+    console.log('updated');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount');
+  },
+  unmounted() {
+    console.log('unmounted');
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.color-red {
+  color: red;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.underline {
+  text-decoration: underline;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  list-style: none;
 }
 </style>
